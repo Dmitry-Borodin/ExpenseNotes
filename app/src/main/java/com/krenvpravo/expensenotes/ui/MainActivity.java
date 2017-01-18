67c392c988eeec73cb4621f1952973d1bf3e251f
Dmitry <mailborodin@gmail.com>
2017-01-18 19:35:17 +0700

    Added dependencies

--- app/src/main/java/com/krenvpravo/expensenotes/ui/MainActivity.java
+++ app/src/main/java/com/krenvpravo/expensenotes/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.krenvpravo.expensenotes;
+package com.krenvpravo.expensenotes.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.krenvpravo.expensenotes.R;
 
 public class MainActivity extends AppCompatActivity {
 
