468f68d0815fd01ab49dfcc639a33904a57be6c1
Dmitry <mailborodin@gmail.com>
2017-01-19 16:24:05 +0700

    small refactoring and improvements

--- app/src/main/java/com/krenvpravo/expensenotes/models/Currency.kt
+++ app/src/main/java/com/krenvpravo/expensenotes/models/Currency.kt
@@ -4,6 +4,7 @@ package com.krenvpravo.expensenotes.models
  * @author Dmitry Borodin on 2017-01-17.
  */
 data class Currency(
+        //ISO-4217
         val index: Int,
         val suffix: String,
         val fullName: String
--- app/src/main/java/com/krenvpravo/expensenotes/models/Transaction.kt
+++ app/src/main/java/com/krenvpravo/expensenotes/models/Transaction.kt
@@ -1,5 +1,7 @@
 package com.krenvpravo.expensenotes.models
 
+import java.util.*
+
 /**
  * @author Dmitry Borodin on 2017-01-17.
  */
@@ -7,7 +9,8 @@ data class Transaction(
         val amoutnWithCents: Long,
         val currency: Currency,
         val description: String,
-        val category: Category? = null
+        val category: Category? = null,
+        val addingDate: Date
 ) {
 
 }
